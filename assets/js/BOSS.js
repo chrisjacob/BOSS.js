c723e769bfdb0feae625ba0911e4722e4736fad0
Chris Jacob <i.chris.jacob@gmail.com>
2011-09-06 23:48:57 +1000

    loading and loaded classes + test styles

--- assets/js/BOSS.js
+++ assets/js/BOSS.js
@@ -1,34 +1,67 @@
-// There's a new BOSS in town; you're end-user!
-var BOSS = {};
 
-// During development bind uncompressed Modernizr to uncompress yepnope.
-// Useful for when you move to the "production" version of Modernizr (with the Modernizr.load "extra") 
-Modernizr.load = function(needs){
-  return yepnope(needs);
-};
+window.BOSS = (function( window, document, undefined ){
 
-// #basic hash (url fragment identifier) can be used to force BASIC mode on an ADVANCED browser
-BOSS.mode = (location.hash == '#basic') ? 'basic' : null;
-
-// ADVANCED mode requires HTML5 Media Query's AND localStorage
-if( BOSS.mode !== 'basic' 
-    && Modernizr.mq('(min-width: 0px)') 
-    && Modernizr.localstorage )
-{
-  BOSS.mode = 'advanced';
+  var 
+  version = '0.0.1',
+  BOSS = {
+    // #basic hash (url fragment identifier) can be used to force BASIC mode on an ADVANCED browser
+    'mode': (location.hash == '#basic') ? 'basic' : null,
+    'load': []
+  },
+  docElement = document.documentElement,
+  docHead = document.head || document.getElementsByTagName('head')[0],
+  elementAddClass = function( element, addClass ){
+    element.className = element.className + ' ' + addClass;
+  },
+  elementReplaceClass = function( element, replaceClass, addClass ){
+    element.className = element.className.replace(replaceClass, addClass);
+  };
   
-  Modernizr.load([
-    'assets/css/advanced.default.css',
-    'assets/less/advanced.custom.less',
-    'assets/js/less-1.1.4.__edited.js'
-  ]);
-}
-else
-{
-  BOSS.mode = 'basic';
-  
-  Modernizr.load('assets/css/basic.default.css');
-}
+  // During development bind uncompressed Modernizr to uncompress yepnope.
+  // Useful for when you move to the "production" version of Modernizr (with the Modernizr.load "extra") 
+  Modernizr.load = function(needs){
+    return yepnope(needs);
+  };
 
-// Debug
-console.log(BOSS);
+  // ADVANCED mode requires HTML5 Media Query's AND localStorage
+  if( BOSS.mode !== 'basic' 
+      && Modernizr.mq('(min-width: 0px)') 
+      && Modernizr.localstorage )
+  {
+    BOSS.mode = 'advanced';
+    elementAddClass(docElement, BOSS.mode);
+  
+    BOSS.load = [
+      'assets/css/advanced.default.css',
+      'assets/less/advanced.custom.less',
+      'assets/js/less-1.1.4.__edited.js'
+    ];
+  }
+  else
+  {
+    BOSS.mode = 'basic';
+    elementAddClass(docElement, BOSS.mode);
+
+    BOSS.load = [
+      'assets/css/basic.default.css'
+    ];
+  }
+  
+  // Final resource to load triggers the "loaded" styling
+  BOSS.load.push({
+    load: 'assets/css/load.loaded.css',
+    complete: function(){
+      // Replace "loading" class with "loaded" on <html> element, if it exists:
+      elementReplaceClass(docElement, 'loading', 'loaded')
+    }
+  });
+  
+  // Get the party started
+  Modernizr.load(BOSS.load);
+
+  // Debug
+  console.log(BOSS);
+
+  return BOSS;
+
+})(this, this.document);
