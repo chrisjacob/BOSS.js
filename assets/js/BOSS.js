a83c38c58357cd5b9c9d4653bb9c0fb5e0bbbc7c
Chris Jacob <i.chris.jacob@gmail.com>
2011-09-11 02:26:34 +1000

    catch "storage" events in active window

--- assets/js/BOSS.js
+++ assets/js/BOSS.js
@@ -7,10 +7,12 @@ window.BOSS = (function( window, document, undefined ){
   BOSS = {
     // #basic hash (url fragment identifier) can be used to force BASIC mode on an ADVANCED browser
     'mode': (location.hash == '#basic') ? 'basic' : null,
-    'load': []
+    'load': [],
+    'data': {}
   },
   docElement = document.documentElement,
   docHead = document.head || document.getElementsByTagName('head')[0],
+  iframeElem = document.createElement('iframe'), 
   elementAddClass = function( element, addClass ){
     element.className = element.className + ' ' + addClass;
   },
@@ -25,7 +27,23 @@ window.BOSS = (function( window, document, undefined ){
     }
     // Consider following this with the loading of "defered" resources
     // i.e. things not critical to display/interact with the page immediately.
+  },
+  storageEvent = function(event) {
+    // Handle changes to localStorage
+    console.log(event.key, event);
   };
+  
+  // Listen for localStorage items being added/updated/deleted in from other windows
+  window.addEventListener('storage', storageEvent, false);
+  
+  // Setup a proxy iframe to trigger the "storage" event within the current window
+  // A proxy is required because browsers only trigger the "storage" event from "other" windows
+  // The iframe src can target a 404 page (as long as it's on the same domain) and this still works
+  // http://html5doctor.com/storing-data-the-simple-html5-way-and-a-few-tricks-you-might-not-have-known/
+  iframeElem = iframeElem;
+  iframeElem.style.display = "none";
+  iframeElem.src = '/storage-proxy-url-can-haz-404';
+  docElement.appendChild(iframeElem);
   
   // During development bind uncompressed Modernizr to uncompress yepnope.
   // Useful for when you move to the "production" version of Modernizr (with the Modernizr.load "extra") 
@@ -53,8 +71,21 @@ window.BOSS = (function( window, document, undefined ){
         complete: function () {
           if ( !window.jQuery ) {
             // .load()'s that follow will wait for the jQuery fallback to load and execute if it needs to.
-            Modernizr.load('assets/js/jquery-1.6.2.min.js');
+            Modernizr.load('assets/js/lib/jquery/jquery-1.6.3.min.js');
           }
+        }
+      },
+      {
+        test: window.JSON,
+        nope: 'assets/js/lib/JSON-js/json2.js',
+        complete: function () {
+          if(localStorage.BOSS !== undefined)
+          {
+            BOSS.data = JSON.parse(localStorage.BOSS);
+            BOSS.data.previousVisit = BOSS.data.timestamp;
+          }
+          BOSS.data.timestamp = new Date().getTime();
+          iframeElem.contentWindow.localStorage.BOSS = JSON.stringify(BOSS.data);
         }
       },
       'assets/js/advanced.js'
