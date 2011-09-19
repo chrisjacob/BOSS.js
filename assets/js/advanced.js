64cda8d6d2c54a31e9614c60f2993b5f052beabe
Chris Jacob <i.chris.jacob@gmail.com>
2011-09-20 02:08:19 +1000

    * clean up

--- assets/js/advanced.js
+++
@@ -1,3 +0,0 @@
-// advanced.js
-
-console.log('advanced.js');
--- assets/js/basic.js
+++
@@ -1,7 +0,0 @@
-// basic.js
-
-console.log('basic.js');
-
-// Suggest not loading any javascript for "BASIC" state... 
-// This will encourage progressive enhancment and accessibility
-// It will also speed up page load
